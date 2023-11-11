a512db8d8a602bc7a9bdb5bacf98f2a6619a7e1c
Mr Mask <ldaiwa7@gmail.com>
2023-11-11 10:56:16 -0500

    update to SQLite to add "Description column"

---
+++ Migrations/20231111155410_AddBookDescription.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace home_lib.Migrations
+{
+    public partial class AddBookDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Books",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Books");
+        }
+    }
+}
